49900c25e9730f42efc9a34308a92d81036f3663
Edenilsonjak <edenilsonjak@gmail.com>
2022-10-14 16:45:42 -0300

    add prisma e db pull

--- backend/api-estabelecimento/src/app.module.ts
+++ backend/api-estabelecimento/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EstabelecimentoModule } from './estabelecimento/estabelecimento.module';
 
 @Module({
-  imports: [],
+  imports: [EstabelecimentoModule],
   controllers: [AppController],
   providers: [AppService],
 })
